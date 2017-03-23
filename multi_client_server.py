0c276067786bd135ee5f529623d59a4d7bc54c29
omer smorodinsky <omer.smorodinsky@gmail.com>
2017-03-23 17:33:44 +0200

    ok

--- multi_client_server.py
+++ multi_client_server.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import socket
 from threading import Thread
 from PIL import ImageGrab
@@ -36,8 +35,6 @@ class Server(object):
         self.server_stream_socket.bind((SERVER_IP, CLIENT_STREAM_PORT))
         self.server_stream_socket.listen(MAX_CLIENTS)
 
-        self.clients_queues = list()
-
         self.client_function_class = ClientFunctions()
 
     def start(self):
@@ -57,10 +54,10 @@ class Server(object):
             client_stream_socket, client_address = self.server_stream_socket.accept()
             client_socket, client_address = self.server_socket.accept()
             print "new client"
-            self.client_function_class.receive_msgs_from_new_clients(client_socket)
+            self.client_function_class.open_chat(client_socket)
 
     def creating_client_queue(self):
-        #TODO
+
 
 
 class ClientFunctions(object):
@@ -95,20 +92,6 @@ class ClientFunctions(object):
         while True:
             client_socket.send(raw_input("insert your msg here..."))
 
-    def receive_msgs_from_new_clients(self, client_socket):
-        """
-        Input: The client socket.
-        Description: When a new client is connected to the server, 2 threads
-                     are opened(self.send_a_msg_to_a_client_thread,
-                     self.receive_a_msg_from_a_client)
-        """
-        client_data = ClientData(client_socket)
-        self.clients_data.append(client_data)
-        receiving_a_msg_from_client_thread = Thread(target=self.receive_a_msg_from_a_client_thread, args=[client_data])
-        receiving_a_msg_from_client_thread.start()
-        sending_a_msg_to_the_client_thread = Thread(target=self.send_a_msg_to_a_client_thread, args=[client_data])
-        sending_a_msg_to_the_client_thread.start()
-
     @staticmethod
     def receive_a_msg_from_a_client_thread(client_data):
         """
@@ -138,29 +121,11 @@ class ClientData(object):
     """
     A Class to hold the information of a client.
     """
-    def __init__(self, client_socket):
+    def __init__(self, client_socket, client_stream_socket, client_address, stream_queue):
         self.socket = client_socket
-        self.hostname = None
-        self.change_client_host_name()
-
-    def change_client_host_name(self):
-        """
-        The clients host name initializes as None.
-        The function receiving the client's host name using the host name protocol from the client and
-        changes the self.hostname to the clients hostname.
-        """
-        host_name_len = self.socket.recv(HOST_NAME_LEN_RECEIVED_SIZE)
-        self.socket.send(OK_RESPONSE)
-        while len(host_name_len) < HOST_NAME_LEN_RECEIVED_SIZE:
-            host_name_len += self.socket.recv(HOST_NAME_LEN_RECEIVED_SIZE - host_name_len)
-        host_name_len = int(host_name_len)
-        self.hostname = self.socket.recv(host_name_len)
-
-    def get_client_host_name(self):
-        """
-        Output: The client's host name.
-        """
-        return self.hostname
+        self.stream_socket = client_stream_socket
+        self.address = client_address
+        self.stream_queue = stream_queue
 
 
 class SessionWithGui(object):
@@ -193,157 +158,3 @@ if __name__ == "__main__":
     da_server = Server()
     da_server.start()
     #ClientFunctions.screen_shot()
-=======
-import socket
-from threading import Thread
-from PIL import ImageGrab
-import time
-import pyHook
-import base64
-
-
-SERVER_IP = "127.0.0.1"
-PORT = 69
-MAX_CLIENTS = 20
-DATA_RECEIVED_SIZE = 1024
-HOST_NAME_LEN_RECEIVED_SIZE = 4
-OK_RESPONSE = "OK"
-NOT_OK_RESPONSE = "SOMETHING WENT WRONG"
-
-
-class Server(object):
-    """
-    Server Class.
-    The server to the teacher-student program.
-    The server has many functions for the Treads Class to use, and many functions for the user to use.
-    """
-    def __init__(self):
-        self.server_socket = socket.socket()
-        self.server_socket.bind((SERVER_IP, PORT))
-        self.server_socket.listen(MAX_CLIENTS)
-        self.client_function_class = ClientFunctions()
-
-    def start(self):
-        """
-        Starting the program.
-        Opens a thread (self.accept_client).
-        """
-        waiting_for_clients = Thread(target=self.accept_client)
-        waiting_for_clients.start()
-
-    def accept_client(self):
-        """
-        A function to a thread that always connects a client to the server.
-        Adds the client to the clients list.
-        """
-        while True:
-            client_socket, client_address = self.server_socket.accept()
-            print "new client"
-            self.client_function_class.receive_msgs_from_new_clients(client_socket)
-
-
-class ClientFunctions(object):
-    """
-    A Class to organize all the clients functions, for the Server Class to use.
-    """
-    def __init__(self):
-        self.clients_data = []
-
-    def open_chat(self, client_socket):
-        """
-        Input: The client socket.
-        Description: When a new client is connected to the server, 2 threads
-                     are opened(self.send_a_msg_to_a_client_thread,
-                     self.receive_a_msg_from_a_client).
-        """
-        client_data = ClientData(client_socket)
-        self.clients_data.append(client_data)
-        receiving_msg_from_client_thread = Thread(target=self.receive_a_msg_from_a_client_thread, args=[client_data])
-        receiving_msg_from_client_thread.start()
-        sending_msg_to_client_thread = Thread(target=self.send_a_msg_to_a_client_thread, args=[client_data])
-        sending_msg_to_client_thread.start()
-
-    @staticmethod
-    def send_a_msg_to_a_client_thread(client_data):
-        """
-        Input: A client's data.
-        Description: A function for a thread that waits for the user to
-                     insert msgs to send to the client, and sends the msgs.
-        """
-        client_socket = client_data.socket
-        while True:
-            client_socket.send(raw_input("insert your msg here..."))
-
-    def receive_msgs_from_new_clients(self, client_socket):
-        """
-        Input: The client socket.
-        Description: When a new client is connected to the server, 2 threads
-                     are opened(self.send_a_msg_to_a_client_thread,
-                     self.receive_a_msg_from_a_client)
-        """
-        client_data = ClientData(client_socket)
-        self.clients_data.append(client_data)
-        receiving_a_msg_from_client_thread = Thread(target=self.receive_a_msg_from_a_client_thread, args=[client_data])
-        receiving_a_msg_from_client_thread.start()
-        sending_a_msg_to_the_client_thread = Thread(target=self.send_a_msg_to_a_client_thread, args=[client_data])
-        sending_a_msg_to_the_client_thread.start()
-
-    @staticmethod
-    def receive_a_msg_from_a_client_thread(client_data):
-        """
-        Input: The client socket.
-        Description: A function for a thread that waits for
-                     data from the client socket and prints it.
-        """
-        client_socket = client_data.socket
-        msg_from_client = None
-        while msg_from_client != "":
-            msg_from_client = client_socket.recv(DATA_RECEIVED_SIZE)
-            print msg_from_client
-
-    @staticmethod
-    def screen_shot():
-        """
-        Takes a screen shot and saves it as a StringIO.
-        Return: The data of the image.
-        """
-        import StringIO
-        string_io = StringIO.StringIO()
-        ImageGrab.grab().save(string_io, "JPEG")
-        return base64.b64encode(string_io.getvalue(), 'utf-8')
-
-
-class ClientData(object):
-    """
-    A Class to hold the information of a client.
-    """
-    def __init__(self, client_socket):
-        self.socket = client_socket
-        self.hostname = None
-        self.change_client_host_name()
-
-    def change_client_host_name(self):
-        """
-        The clients host name initializes as None.
-        The function receiving the client's host name using the host name protocol from the client and
-        changes the self.hostname to the clients hostname.
-        """
-        host_name_len = self.socket.recv(HOST_NAME_LEN_RECEIVED_SIZE)
-        self.socket.send(OK_RESPONSE)
-        while len(host_name_len) < HOST_NAME_LEN_RECEIVED_SIZE:
-            host_name_len += self.socket.recv(HOST_NAME_LEN_RECEIVED_SIZE - host_name_len)
-        host_name_len = int(host_name_len)
-        self.hostname = self.socket.recv(host_name_len)
-
-    def get_client_host_name(self):
-        """
-        Output: The client's host name.
-        """
-        return self.hostname
-
-
-if __name__ == "__main__":
-    da_server = Server()
-    da_server.start()
-    #ClientFunctions.screen_shot()
->>>>>>> 4be47f6a7a728a652073f717dee301a6aa39c9e0
