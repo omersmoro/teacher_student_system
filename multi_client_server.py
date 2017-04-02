f551bd9f2cfa5db938d30a6304de2cd2b8282f49
omersmoro <omer.smorodinsky@gmail.com>
2017-04-02 21:41:58 +0300

    working on session with gui

--- multi_client_server.py
+++ multi_client_server.py
@@ -1,12 +1,26 @@
 import socket
 from threading import Thread
+from PIL import ImageGrab
+import time
+import base64
+import Queue
 
 
-SERVER_IP = "127.0.0.1"
-PORT = 80
+SERVER_IP = "0.0.0.0"
+LOCAL_IP = "127.0.0.1"
+
+CLIENT_PORT = 1025
+CLIENT_STREAM_PORT = 1026
+
+LOCAL_PORT = 1027
+LOCAL_STREAM_PORT = 1028
+
 MAX_CLIENTS = 20
 DATA_RECEIVED_SIZE = 1024
 HOST_NAME_LEN_RECEIVED_SIZE = 4
+
+OK_RESPONSE = "OK"
+NOT_OK_RESPONSE = "SOMETHING WENT WRONG"
 
 
 class Server(object):
@@ -16,11 +30,18 @@ class Server(object):
     The server has many functions for the Treads Class to use, and many functions for the user to use.
     """
     def __init__(self):
+
         self.server_socket = socket.socket()
-        self.server_socket = socket.socket()
-        self.server_socket.bind((SERVER_IP, PORT))
+        self.server_socket.bind((SERVER_IP, CLIENT_PORT))
         self.server_socket.listen(MAX_CLIENTS)
-        self.client_function_class = ClientFunctions()
+
+        self.server_stream_socket = socket.socket()
+        self.server_stream_socket.bind((SERVER_IP, CLIENT_STREAM_PORT))
+        self.server_stream_socket.listen(MAX_CLIENTS)
+
+        self.client_function_class = SessionWithClient()
+
+        self.session_with_gui_class = SessionWithGui()
 
     def start(self):
         """
@@ -36,80 +57,143 @@ class Server(object):
         Adds the client to the clients list.
         """
         while True:
-            client_socket, client_address = self.server_socket.accept()
-            print "new client"
-            self.client_function_class.receive_msgs_from_new_clients_thread(client_socket)
+            if len(self.client_function_class.clients_data) < MAX_CLIENTS:
 
-    def open_chat(self):
-        """
-        """
+                client_stream_socket, client_address = self.server_stream_socket.accept()
+                client_socket, client_address = self.server_socket.accept()
+
+                gui_client_socket = socket.socket()
+                gui_client_socket.connect((LOCAL_IP, LOCAL_STREAM_PORT))
+
+                print "new client"
+                self.client_function_class.open_chat(client_socket, client_stream_socket,
+                                                     client_address, gui_client_socket)
+            else:
+                print "Max clients reached, can't add more clients."
+                break
 
 
-class ClientFunctions(object):
+class SessionWithClient(object):
     """
     A Class to organize all the clients functions, for the Server Class to use.
     """
     def __init__(self):
         self.clients_data = []
 
-    def send_msg_thread(self, client_host_name):
-        """
-        Input: A client host name to check which client to open a chat with.
-        Description: checks to see with which client the server wants to open a chat,
-                     and opens the chat.
-        """
-        for client_socket in self.clients_data:
-            if client_socket.host_name == client_host_name:
-                client_socket.send(raw_input("insert your msg here..."))
-
-    def receive_msgs_from_new_clients_thread(self, client_socket):
+    def open_chat(self, client_socket, client_stream_socket, client_address, gui_client_socket):
         """
         Input: The client socket.
-        Description: When a new client is connected to the server, a thread
-                     is opened that waits for a msg from a client.
+        Description: When a new client is connected to the server, 3 threads are
+                     opened(self.send_a_msg_to_a_client_thread, self.receive_a_msg_from_a_client,
+                     self.connecting_stream_from_client_to_gui).
         """
-        client_data = ClientData(client_socket)
+        client_data = ClientData(client_socket, client_stream_socket, client_address, gui_client_socket)
         self.clients_data.append(client_data)
-        receiving_msg_from_client_thread = Thread(target=self.receive_a_msg_from_a_client, args=[client_data])
+
+        receiving_msg_from_client_thread = Thread(target=self.receive_a_msg_from_a_client_thread, args=[client_data])
         receiving_msg_from_client_thread.start()
 
+        sending_msg_to_client_thread = Thread(target=self.send_a_msg_to_a_client_thread, args=[client_data])
+        sending_msg_to_client_thread.start()
+
+        receiving_stream_from_client_thread = Thread(target=self.connecting_stream_from_client_to_gui, args=[client_data])
+        receiving_stream_from_client_thread.start()
+
     @staticmethod
-    def receive_a_msg_from_a_client(client_data):
+    def send_a_msg_to_a_client_thread(client_data):
         """
-        Input: The client socket.
+        Input: A client's data.
+        Description: A function for a thread that waits for the user to
+                     insert msgs to send to the client, and sends the msgs.
+        """
+        client_socket = client_data.socket
+        while True:
+            client_socket.send(raw_input("insert your msg here..."))
+
+    @staticmethod
+    def receive_a_msg_from_a_client_thread(client_data):
+        """
+        Input: A client's data.
         Description: A function for a thread that waits for
                      data from the client socket and prints it.
         """
-        client_data.get_client_host_name()
         client_socket = client_data.socket
-        while True:
+        msg_from_client = None
+        while msg_from_client != "":
             msg_from_client = client_socket.recv(DATA_RECEIVED_SIZE)
             print msg_from_client
+
+    @staticmethod
+    def screen_shot():
+        """
+        Takes a screen shot and saves it as a StringIO.
+        Return: The data of the image.
+        """
+        import StringIO
+        string_io = StringIO.StringIO()
+        ImageGrab.grab().save(string_io, "JPEG")
+        return base64.b64encode(string_io.getvalue(), 'utf-8')
+
+    def connecting_stream_from_client_to_gui(self, client_data):
+        """
+        Input: A client's data.
+        Description: A function for a thread that gets the stream of
+                     a client and sends it to the gui.
+        """
+        client_stream_socket = client_data.client_stream_socket
+        while True:
+            len_of_img = client_stream_socket.recv()
+            img = self.get_full_size_data(len_of_img, client_stream_socket)
+            client_data.gui_stream_socket.send(img)
+
+    @staticmethod
+    def get_full_size_data(data_len, client_stream_socket):
+        """
+        #NOT IN USE
+        Input: The length of the data that needs to be received.
+        Output: The data that was received.
+        description: A function that receives data from the server, and checks to see if all the data has been received.
+                     If not, it waits until all the data was received.
+        """
+        data = client_stream_socket.recv(data_len)
+        while len(data) < data_len:
+            data += client_stream_socket.recv(data_len-len(data))
+        return data
 
 
 class ClientData(object):
     """
     A Class to hold the information of a client.
     """
-    def __init__(self, socket):
-        self.socket = socket
-        self.hostname = None
+    def __init__(self, client_socket, client_stream_socket, client_address, gui_stream_socket):
+        self.socket = client_socket
+        self.stream_socket = client_stream_socket
+        self.address = client_address
+        self.gui_stream_socket = gui_stream_socket
 
-    def get_client_host_name(self):
+
+class SessionWithGui(object):
+    """
+    A class to communicate with the gui (the main socket, not the stream sockets).
+    """
+    def __init__(self):
+        self.server_socket = socket.socket()
+        self.server_socket.bind((LOCAL_IP, LOCAL_PORT))
+        self.server_socket.listen()
+
+    def connect_to_gui(self):
         """
-        Output: The client's host name.
-        Receiving the client's host name using the host name protocol.
+        Connect to the gui with the socket (with both socket).
         """
-        host_name_len = self.socket.recv(HOST_NAME_LEN_RECEIVED_SIZE)
-        while len(host_name_len) < HOST_NAME_LEN_RECEIVED_SIZE:
-            host_name_len += self.socket.recv(HOST_NAME_LEN_RECEIVED_SIZE - host_name_len)
-        host_name_len = int(host_name_len)
-        self.hostname = self.socket.recv(host_name_len)
-        return self.hostname
+        try:
+            gui_socket, gui_address = self.server_socket.accept()
+            return gui_socket
+
+        except socket.error:
+            print str(socket.error)
+
 
 
 if __name__ == "__main__":
     da_server = Server()
     da_server.start()
-    if raw_input("send a msg?") == "yes":
-        da_server.client_function_class.send_msg(da_server.client_function_class.clients[0].gethostname())
