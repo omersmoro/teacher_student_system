d54d78e08f0d5cc39a424e593166b0fb354bf61d
omersmoro <omer.smorodinsky@gmail.com>
2017-05-17 19:47:13 +0300

    stream working, need to check on 2 computers.

--- multi_client_server.py
+++ multi_client_server.py
@@ -112,14 +112,13 @@ class SessionWithClient(object):
             len_of_img, client_address = client_stream_socket.recvfrom(DATA_RECEIVED_SIZE)
             len_of_img = self.change_int_to_7_length(len_of_img)
             img = self.get_full_size_data(int(len_of_img), client_stream_socket)
-            print base64.b64decode(img)
             print len(img)
             self.session_with_gui_class.stream_socket.send(client_data.address)
+            time.sleep(0.1)
             self.session_with_gui_class.stream_socket.send(len_of_img)
             while img:
                 self.session_with_gui_class.stream_socket.send(img[:1024])
                 img = img[1024:]
-            print "img sent"
 
     def send_a_msg_to_a_client(self, ip, text):
         """
